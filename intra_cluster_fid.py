a4c4478d2be09ae374e123f552b70bb73b49a186
SLOGAN-anonymous <85322658+SLOGAN-anonymous@users.noreply.github.com>
2021-06-04 22:42:32 +0900

    Add files via upload

--- intra_cluster_fid.py
+++ intra_cluster_fid.py
@@ -62,7 +62,7 @@ def get_activations(images, sess, batch_size=50, verbose=False):
     n_batches = d0 // batch_size
     n_used_imgs = n_batches * batch_size
     pred_arr = np.empty((n_used_imgs, 2048))
-    for i in tqdm(range(n_batches)):
+    for i in range(n_batches):
         if verbose:
             print("\rPropagating batch %d/%d" % (i + 1, n_batches))
         start = i * batch_size
